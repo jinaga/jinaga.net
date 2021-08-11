36bc198f8ec9b2887e72fc7b64d483c132b16099
Michael L Perry <michael@qedcode.com>
2021-08-10 22:11:14 -0500

    Render facts as a graph by shelling out to dot.

--- Jinaga.Graphviz/Renderer.cs
+++ Jinaga.Graphviz/Renderer.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.IO;
 using System.Linq;
+using System.Text.Json;
 using Jinaga.Facts;
 using Jinaga.Serialization;
 using Microsoft.AspNetCore.Html;
@@ -24,26 +24,57 @@ namespace Jinaga.Graphviz
                 "}"
             };
             var collector = new Collector(SerializerCache.Empty);
+            var references = new List<FactReference>();
             foreach (var fact in facts)
             {
-                collector.Serialize(fact);
+                var reference = collector.Serialize(fact);
+                references.Add(reference);
             }
             var body = collector.Graph.FactReferences
-                .SelectMany(reference => FactToDigraph(collector.Graph.GetFact(reference)));
+                .SelectMany(reference => FactToDigraph(collector.Graph.GetFact(reference), references.Contains(reference)));
             string graph = string.Join("\n", prefix.Concat(body).Concat(suffix));
             return RenderGraph(graph);
         }
 
-        private static IEnumerable<string> FactToDigraph(Fact fact)
+        private static IEnumerable<string> FactToDigraph(Fact fact, bool hilight)
         {
-            yield return NodeLine(fact);
+            yield return NodeLine(fact, hilight);
+            foreach (var predecessor in fact.Predecessors)
+            {
+                var references = predecessor switch
+                {
+                    PredecessorSingle single => new[] { single.Reference },
+                    PredecessorMultiple multiple => multiple.References,
+                    _ => Enumerable.Empty<FactReference>()
+                };
+                foreach (var reference in references)
+                {
+                    yield return $"\"{fact.Reference.Hash}\" -> \"{reference.Hash}\" [label=\"{predecessor.Role}\"]";
+                }
+            }
         }
 
-        private static string NodeLine(Fact fact)
+        private static string NodeLine(Fact fact, bool hilight)
         {
-            string factRows = @$"<TR><TD COLSPAN=""2"">{Uri.EscapeUriString(fact.Reference.Type)}</TD></TR>";
-            string factLabel = @$"<TABLE BORDER=""0"" CELLBORDER=""1"" CELLSPACING=""0"">{factRows}</TABLE>";
+            var fieldRows = fact.Fields
+                .Select(field => $"<TR><TD>{Uri.EscapeUriString(field.Name)}</TD><TD>{SerializeFieldValue(field.Value)}</TD></TR>")
+                .ToArray();
+            var fieldText = String.Join("", fieldRows);
+            string typeRow = @$"<TR><TD COLSPAN=""2"">{Uri.EscapeUriString(fact.Reference.Type)}</TD></TR>";
+            int border = hilight ? 1 : 0;
+            string factLabel = @$"<TABLE BORDER=""{border}"" CELLBORDER=""1"" CELLSPACING=""0"">{typeRow}{fieldText}</TABLE>";
             return $"\"{fact.Reference.Hash}\" [label=<{factLabel}>]";
+        }
+        
+        private static string SerializeFieldValue(FieldValue value)
+        {
+            return value switch
+            {
+                FieldValueString str => JsonSerializer.Serialize(str.StringValue),
+                FieldValueNumber number => JsonSerializer.Serialize(number.DoubleValue),
+                FieldValueBoolean b => JsonSerializer.Serialize(b.BoolValue),
+                _ => throw new NotImplementedException()
+            };
         }
 
         private static HtmlString RenderGraph(string graph)
