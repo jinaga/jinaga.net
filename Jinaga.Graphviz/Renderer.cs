5394f75befc6269249355e005c3504f418e0264b
Michael L Perry <michael@qedcode.com>
2021-08-22 16:57:24 -0500

    Render type graphs

--- Jinaga.Graphviz/Renderer.cs
+++ Jinaga.Graphviz/Renderer.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using Jinaga.Facts;
+using Jinaga.Parsers;
 using Jinaga.Serialization;
 using Microsoft.AspNetCore.Html;
 
@@ -75,6 +78,95 @@ namespace Jinaga.Graphviz
                 FieldValueBoolean b => JsonSerializer.Serialize(b.BoolValue),
                 _ => throw new NotImplementedException()
             };
+        }
+
+        public static ImmutableList<string> ListTypes(params Type[] types)
+        {
+            var toVisit = types
+                .Where(t => t.GetCustomAttributes(typeof(FactTypeAttribute), false).Any())
+                .ToImmutableList();
+            var visited = ImmutableList<Type>.Empty;
+            var lines = ImmutableList<string>.Empty;
+            while (toVisit.Any())
+            {
+                (toVisit, visited, lines) = VisitFactType(toVisit, visited, lines);
+            }
+            return lines;
+        }
+
+        public static HtmlString RenderTypes(params Type[] types)
+        {
+            string[] prefix = new[]
+            {
+                "digraph {",
+                "rankdir=BT",
+            };
+            string[] suffix = new[]
+            {
+                "}"
+            };
+            var toVisit = types
+                .Where(t => t.GetCustomAttributes(typeof(FactTypeAttribute), false).Any())
+                .ToImmutableList();
+            var visited = ImmutableList<Type>.Empty;
+            var lines = ImmutableList<string>.Empty;
+            while (toVisit.Any())
+            {
+                (toVisit, visited, lines) = VisitFactType(toVisit, visited, lines);
+            }
+            string graph = string.Join("\n", prefix.Concat(lines).Concat(suffix));
+            return RenderGraph(graph);
+        }
+
+        private static (ImmutableList<Type> toVisit, ImmutableList<Type> visited, ImmutableList<string> lines) VisitFactType(ImmutableList<Type> toVisit, ImmutableList<Type> visited, ImmutableList<string> lines)
+        {
+            var factClass = toVisit.First();
+            var predecessors =
+                from property in factClass.GetProperties(BindingFlags.Public | BindingFlags.Instance)
+                from predecessorType in GetFactType(property.PropertyType)
+                select new
+                {
+                    Name = property.Name,
+                    Type = property.PropertyType,
+                    FactType = property.PropertyType.FactTypeName()
+                };
+            
+            var left = factClass.FactTypeName();
+            var newLines = predecessors
+                .Select(predecessor => $"\"{left}\" -> \"{predecessor.FactType}\" [label=\"{predecessor.Name}\"]");
+            var newToVisit = predecessors
+                .Select(predecessor => predecessor.Type)
+                .Where(type => !toVisit.Contains(type) && !visited.Contains(type));
+            
+            return (toVisit.Skip(1).Concat(newToVisit).ToImmutableList(), visited.Add(factClass), lines.AddRange(newLines));
+        }
+
+        private static IEnumerable<string> FactClassToDigraph(Type factClass)
+        {
+            var left = factClass.FactTypeName();
+            var properties = factClass.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var predecessorList =
+                from property in properties
+                let role = property.Name
+                from right in GetFactType(property.PropertyType)
+                select $"{left} -> {right} [label=\"{role}\"]";
+            return predecessorList;
+        }
+
+        private static IEnumerable<Type> GetFactType(Type type)
+        {
+            if (type.IsArray)
+            {
+                return GetFactType(type.GetElementType());
+            }
+            else if (type.GetCustomAttributes<FactTypeAttribute>(inherit: false).Any())
+            {
+                return new [] { type };
+            }
+            else 
+            {
+                return new Type[0];
+            }
         }
 
         private static HtmlString RenderGraph(string graph)
