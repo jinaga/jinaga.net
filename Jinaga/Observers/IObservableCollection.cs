55df9edebdfb2e7c82513a9884deee06e996b7f4
Michael L Perry <michael@qedcode.com>
2021-09-06 20:39:31 -0500

    Convenience overloads of observable collection OnAdded.

--- Jinaga/Observers/IObservableCollection.cs
+++ Jinaga/Observers/IObservableCollection.cs
@@ -7,5 +7,36 @@ namespace Jinaga.Observers
     public interface IObservableCollection<TProjection> : IEnumerable<TProjection>
     {
         void OnAdded(Func<TProjection, Task<Func<Task>>> added);
+
+        void OnAdded(Func<TProjection, Task> added)
+        {
+            this.OnAdded(async projection =>
+            {
+                await added(projection);
+                return () => Task.CompletedTask;
+            });
+        }
+
+        void OnAdded(Func<TProjection, Action> added)
+        {
+            this.OnAdded(projection =>
+            {
+                var removal = added(projection);
+                return Task.FromResult<Func<Task>>(() =>
+                {
+                    removal();
+                    return Task.CompletedTask;
+                });
+            });
+        }
+
+        void OnAdded(Action<TProjection> added)
+        {
+            this.OnAdded(projection =>
+            {
+                added(projection);
+                return Task.FromResult<Func<Task>>(() => Task.CompletedTask);
+            });
+        }
     }
 }
