c6c0805b7bd8d556d0ef90bcbe34c2ffffa4077f
Michael L Perry <michael@qedcode.com>
2021-09-19 21:56:51 -0500

    Fixed product comparison

--- Jinaga/Products/Product.cs
+++ Jinaga/Products/Product.cs
@@ -1,36 +1,49 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
+using System.Linq;
+using Jinaga.Pipelines;
 
 namespace Jinaga.Products
 {
     public class Product
     {
         public static Product Empty = new Product(ImmutableDictionary<string, Element>.Empty);
-        private readonly ImmutableDictionary<string, Element> products;
+        private readonly ImmutableDictionary<string, Element> elements;
 
-        private Product(ImmutableDictionary<string, Element> products)
+        private Product(ImmutableDictionary<string, Element> elements)
         {
-            this.products = products;
+            this.elements = elements;
         }
 
-        public IEnumerable<string> Names => products.Keys;
+        public IEnumerable<string> Names => elements.Keys;
 
-        public Element GetElement(string name) => products[name];
+        public Element GetElement(string name) => elements[name];
 
-        public Product With(string name, Element product)
+        public Product With(string name, Element element)
         {
-            return new Product(products.SetItem(name, product));
+            return new Product(elements.SetItem(name, element));
         }
 
         public override bool Equals(object obj)
         {
-            return obj is Product composite &&
-                EqualityComparer<ImmutableDictionary<string, Element>>.Default.Equals(products, composite.products);
+            if (obj == null || GetType() != obj.GetType())
+            {
+                return false;
+            }
+            
+            var that = (Product)obj;
+            var thoseReferences = that.elements
+                .Select(pair => ComparablePair.From(pair.Key, pair.Value));
+            var theseReferences = this.elements
+                .Select(pair => ComparablePair.From(pair.Key, pair.Value));
+            return thoseReferences.SetEquals(theseReferences);
         }
 
         public override int GetHashCode()
         {
-            return -1937169414 + EqualityComparer<ImmutableDictionary<string, Element>>.Default.GetHashCode(products);
+            var theseReferences = this.elements
+                .Select(pair => ComparablePair.From(pair.Key, pair.Value));
+            return theseReferences.SetHash();
         }
     }
 }
