c0d3aea9a823212e5686fd7c442e73e92adf67f0
Michael L Perry <michael@qedcode.com>
2021-11-07 11:23:41 -0600

    Remove version attributes from code

--- Jinaga.Android/SampleApp.Android/Properties/AssemblyInfo.cs
+++ Jinaga.Android/SampleApp.Android/Properties/AssemblyInfo.cs
@@ -1,7 +1,5 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -15,12 +13,3 @@ using Android.App;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
