c2a0027fcf40d9fa4599d9c040999b5b50963724
Michael L Perry <michael@qedcode.com>
2021-11-12 17:25:37 -0600

    Deserialize collections of products

--- Jinaga.Test/Model/DWS.cs
+++ Jinaga.Test/Model/DWS.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace Jinaga.Test.Model.DWS
 {
@@ -10,13 +11,25 @@ namespace Jinaga.Test.Model.DWS
     public record Client(Supplier supplier, DateTime created);
 
     [FactType("DWS.Client.Name")]
-    public record ClientName(Client client, string name, ClientName[] prior);
-
+    public record ClientName(Client client, string name, ClientName[] prior)
+    {
+        public Condition IsCurrent => new Condition(facts => !(
+            from next in facts.OfType<ClientName>()
+            where next.prior.Contains(this)
+            select next
+        ).Any());
+    }
 
     [FactType("DWS.Yard")]
     public record Yard(Client client, DateTime created);
 
     [FactType("DWS.Yard.Address")]
-    public record YardAddress(Yard yard, string name, string remark, string street, string housNb, string postalCode, string city, string country, YardAddress[] prior);
-
+    public record YardAddress(Yard yard, string name, string remark, string street, string housNb, string postalCode, string city, string country, YardAddress[] prior)
+    {
+        public Condition IsCurrent => new Condition(facts => !(
+            from next in facts.OfType<YardAddress>()
+            where next.prior.Contains(this)
+            select next
+        ).Any());
+    }
 }
