d90287da8a80dee519911029f391ff61b4cf6aed
Jan Verhaegen <jan.verhaegen@tss.be>
2021-11-29 14:17:08 +0100

    Initial setup Jinaga.Store.SQLite

---
+++ Jinaga.Store.SQLite/SQLiteStore.cs
@@ -0,0 +1,29 @@
+﻿using Jinaga.Facts;
+using Jinaga.Products;
+using Jinaga.Projections;
+using Jinaga.Services;
+using System;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jinaga.Store.SQLite
+{
+    public class SQLiteStore : IStore
+    {
+        Task<FactGraph> IStore.Load(ImmutableList<FactReference> references, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ImmutableList<Product>> IStore.Query(ImmutableList<FactReference> startReferences, Specification specification, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ImmutableList<Fact>> IStore.Save(FactGraph graph, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
