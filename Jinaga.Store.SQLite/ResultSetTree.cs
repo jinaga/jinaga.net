9cc5e3e1002bc23ce2b6c0fe06898f786584d8c8
Michael L Perry <michael@qedcode.com>
2023-05-14 16:04:14 -0500

    Convert result sets to products

---
+++ Jinaga.Store.SQLite/ResultSetTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Immutable;
+
+namespace Jinaga.Store.SQLite
+{
+    internal class ResultSetFact
+    {
+        public string Hash { get; set; }
+        public int FactId { get; set; }
+        public string Data { get;set; }
+    }
+
+    internal class ResultSetTree
+    {
+        public ImmutableList<ImmutableDictionary<int, ResultSetFact>> ResultSet { get; set; }
+        public ImmutableDictionary<string, ResultSetTree> ChildResultSets { get; set; }
+    }
+}
