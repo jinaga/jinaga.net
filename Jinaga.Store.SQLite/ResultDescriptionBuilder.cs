a9da6f6b76edb5ebad96760f0b4cd14ac6cf39fb
Michael L Perry <michael@qedcode.com>
2023-05-18 12:59:05 -0500

    Begin result description builder

---
+++ Jinaga.Store.SQLite/ResultDescriptionBuilder.cs
@@ -0,0 +1,48 @@
+﻿using Jinaga.Facts;
+using Jinaga.Projections;
+using System;
+using System.Collections.Immutable;
+
+namespace Jinaga.Store.SQLite
+{
+    internal class QueryDescription
+    {
+        public SpecificationSqlQuery GenerateResultSqlQuery()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSatisfiable()
+        {
+            throw new NotImplementedException();
+        }
+
+        public int OutputLength()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    internal class ResultDescription
+    {
+        public QueryDescription QueryDescription { get; set; }
+        public ImmutableDictionary<string, ResultDescription> ChildResultDescriptions { get; set; }
+    }
+
+    internal class ResultDescriptionBuilder
+    {
+        private ImmutableDictionary<string, int> factTypes;
+        private ImmutableDictionary<int, ImmutableDictionary<string, int>> roleMap;
+
+        public ResultDescriptionBuilder(ImmutableDictionary<string, int> factTypes, ImmutableDictionary<int, ImmutableDictionary<string, int>> roleMap)
+        {
+            this.factTypes = factTypes;
+            this.roleMap = roleMap;
+        }
+
+        internal ResultDescription Build(ImmutableList<FactReference> startReferences, Specification specification)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
