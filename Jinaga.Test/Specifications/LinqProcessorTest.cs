11e4a1cde63292a35177e9f994bfc5c3060d2fb8
Michael L Perry <michael@qedcode.com>
2023-05-21 08:51:39 -0500

    Process a fact source

---
+++ Jinaga.Test/Specifications/LinqProcessorTest.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using Jinaga.Projections;
+using Jinaga.Specifications;
+using Xunit;
+using static Jinaga.Specifications.LinqProcessor;
+
+namespace Jinaga.Test.Specifications;
+public class LinqProcessorTest
+{
+    [Fact]
+    public void CanProcessFactSource()
+    {
+        var source = FactsOfType("Employee");
+
+        var match = source.Matches.Should().ContainSingle().Subject;
+        match.Unknown.Name.Should().Be("***");
+        match.Unknown.Type.Should().Be("Employee");
+        match.Conditions.Should().BeEmpty();
+
+        var projection = source.Projection.Should().BeOfType<SimpleProjection>().Subject;
+        projection.Tag.Should().Be("***");
+    }
+}
---
+++ Jinaga/Specifications/LinqProcessor.cs
@@ -0,0 +1,18 @@
+﻿using Jinaga.Pipelines;
+using Jinaga.Projections;
+using System.Collections.Immutable;
+
+namespace Jinaga.Specifications
+{
+    internal class LinqProcessor
+    {
+        public static SourceContext FactsOfType(string typeName)
+        {
+            var unknown = new Label("***", typeName);
+            var match = new Match(unknown, ImmutableList<MatchCondition>.Empty);
+            var matches = ImmutableList.Create(match);
+            var projection = new SimpleProjection(unknown.Name);
+            return new SourceContext(matches, projection);
+        }
+    }
+}
