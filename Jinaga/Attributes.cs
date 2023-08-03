be01facee73d631aa668c84032eda5baf4bf8b61
Jan Verhaegen <jan.verhaegen@tss.be>
2023-08-03 15:09:31 +0200

    Adapted InternalsVisibleTo to make Jinaga visible for Jinaga.Test and SQLite.Test

--- Jinaga/Attributes.cs
+++ Jinaga/Attributes.cs
@@ -1,2 +1,5 @@
 ﻿// Make internal classes visible to the Jinaga.Store.SQLite.Test project.
 [assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Jinaga.Store.SQLite.Test")]
+
+// Make internal classes visible to the Jinaga.Test project.
+[assembly: System.Runtime.CompilerServices.InternalsVisibleTo("Jinaga.Test")]
