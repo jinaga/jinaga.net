819fc246dead6f37f8759a9ac93e6470bad60d55
Michael L Perry <michael@qedcode.com>
2024-01-18 21:21:12 -0600

    Test version consistency

--- Jinaga.Test/Specifications/ProjectionVersioningTest.cs
+++ Jinaga.Test/Specifications/ProjectionVersioningTest.cs
@@ -38,18 +38,21 @@ public class ProjectionVersioningTest
         var jinagaClient = JinagaTest.Create();
         var site = await jinagaClient.Fact(new Model.Site(new User("Michael"), "michaelperry.net"));
         var contentV1 = await jinagaClient.Fact(new Model.Content(site, "index.html"));
+        var originalHash = jinagaClient.Hash(contentV1);
 
         var contentCreatedAtForSite = Given<Model.Site>.Match((site, facts) =>
             from content in facts.OfType<Model.ContentV2>()
             where content.site == site
             select new
             {
-                content.createdAt
+                content.createdAt,
+                hash = jinagaClient.Hash(content)
             }
         );
         var contentCreatedAt = await jinagaClient.Query(contentCreatedAtForSite, site);
 
         contentCreatedAt.Should().ContainSingle().Which.createdAt.Should().BeNull();
+        contentCreatedAt.Should().ContainSingle().Which.hash.Should().Be(originalHash);
     }
 
     [Fact]
@@ -58,18 +61,21 @@ public class ProjectionVersioningTest
         var jinagaClient = JinagaTest.Create();
         var site = await jinagaClient.Fact(new Model.Site(new User("Michael"), "michaelperry.net"));
         var contentV2 = await jinagaClient.Fact(new Model.ContentV2(site, new DateTime(2021, 1, 1).ToUniversalTime()));
+        var originalHash = jinagaClient.Hash(contentV2);
 
         var contentCreatedAtForSite = Given<Model.Site>.Match((site, facts) =>
             from content in facts.OfType<Model.ContentV2>()
             where content.site == site
             select new
             {
-                content.createdAt
+                content.createdAt,
+                hash = jinagaClient.Hash(content)
             }
         );
         var contentCreatedAt = await jinagaClient.Query(contentCreatedAtForSite, site);
 
         contentCreatedAt.Should().ContainSingle().Which.createdAt.Should().Be(new DateTime(2021, 1, 1).ToUniversalTime());
+        contentCreatedAt.Should().ContainSingle().Which.hash.Should().Be(originalHash);
     }
 
     [Fact]
@@ -78,17 +84,20 @@ public class ProjectionVersioningTest
         var jinagaClient = JinagaTest.Create();
         var site = await jinagaClient.Fact(new Model.Site(new User("Michael"), "michaelperry.net"));
         var contentV2 = await jinagaClient.Fact(new Model.ContentV2(site, new DateTime(2021, 1, 1)));
+        var originalHash = jinagaClient.Hash(contentV2);
 
         var contentPathForSite = Given<Model.Site>.Match((site, facts) =>
             from content in facts.OfType<Model.Content>()
             where content.site == site
             select new
             {
-                content.path
+                content.path,
+                hash = jinagaClient.Hash(content)
             }
         );
         var contentPath = await jinagaClient.Query(contentPathForSite, site);
 
         contentPath.Should().ContainSingle().Which.path.Should().Be("");
+        contentPath.Should().ContainSingle().Which.hash.Should().Be(originalHash);
     }
 }
