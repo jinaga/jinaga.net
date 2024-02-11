5a7edd532cc9e8b84a93722dab81d74c30977159
Michael L Perry <michael@qedcode.com>
2024-02-10 22:29:38 -0600

    Handle different kinds of namespace declaration

--- Jinaga.SourceGenerator/FactGenerator.cs
+++ Jinaga.SourceGenerator/FactGenerator.cs
@@ -38,17 +38,19 @@ namespace Jinaga.SourceGenerator
                 var className = classDeclaration.Identifier.Text;
 
                 // Generate the source code for the other half of the partial class
+                var namespaceDeclaration = string.IsNullOrEmpty(namespaceName) ? "" : $"namespace {namespaceName}\n{{\n";
+                var closingBrace = string.IsNullOrEmpty(namespaceName) ? "" : "\n}";
+
                 var source = $@"
 using Jinaga.Facts;
 using Jinaga.Serialization;
 
-namespace {namespaceName}
-{{
+{namespaceDeclaration}
     public partial class {className} : IFactProxy
     {{
         FactGraph IFactProxy.Graph {{ get; set; }}
     }}
-}}";
+{closingBrace}";
 
                 // Add the source code to the compilation
                 context.AddSource($"{className}.g.cs", SourceText.From(source, Encoding.UTF8));
@@ -64,31 +66,72 @@ namespace {namespaceName}
                 var recordName = recordDeclaration.Identifier.Text;
 
                 // Generate the source code for the other half of the partial record
+                var namespaceDeclaration = string.IsNullOrEmpty(namespaceName) ? "" : $"namespace {namespaceName}\n{{\n";
+                var closingBrace = string.IsNullOrEmpty(namespaceName) ? "" : "\n}";
+
                 var source = $@"
 using Jinaga.Facts;
 using Jinaga.Serialization;
 
-namespace {namespaceName}
-{{
+{namespaceDeclaration}
     public partial record {recordName} : IFactProxy
     {{
         FactGraph IFactProxy.Graph {{ get; set; }}
     }}
-}}";
+{closingBrace}";
 
                 // Add the source code to the compilation
                 context.AddSource($"{recordName}.g.cs", SourceText.From(source, Encoding.UTF8));
             }
         }
 
-        private static string GetNamespaceName(SyntaxNode syntaxNode)
+        // determine the namespace the class/enum/struct is declared in, if any
+        private static string GetNamespaceName(BaseTypeDeclarationSyntax syntax)
         {
-            var namespaceDeclaration = syntaxNode.Parent is NamespaceDeclarationSyntax
-                ? (NamespaceDeclarationSyntax)syntaxNode.Parent
-                : syntaxNode.SyntaxTree.GetRoot().DescendantNodes().OfType<NamespaceDeclarationSyntax>().FirstOrDefault();
+            // If we don't have a namespace at all we'll return an empty string
+            // This accounts for the "default namespace" case
+            string nameSpace = string.Empty;
 
-            var namespaceName = namespaceDeclaration?.Name.ToString();
-            return namespaceName;
+            // Get the containing syntax node for the type declaration
+            // (could be a nested type, for example)
+            SyntaxNode potentialNamespaceParent = syntax.Parent;
+
+            // Keep moving "out" of nested classes etc until we get to a namespace
+            // or until we run out of parents
+            while (potentialNamespaceParent != null)
+            {
+                if (potentialNamespaceParent is NamespaceDeclarationSyntax || potentialNamespaceParent is FileScopedNamespaceDeclarationSyntax)
+                {
+                    break;
+                }
+
+                potentialNamespaceParent = potentialNamespaceParent.Parent;
+            }
+
+            // Build up the final namespace by looping until we no longer have a namespace declaration
+            if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
+            {
+                // We have a namespace. Use that as the type
+                nameSpace = namespaceParent.Name.ToString();
+
+                // Keep moving "out" of the namespace declarations until we 
+                // run out of nested namespace declarations
+                while (true)
+                {
+                    var parent = namespaceParent.Parent as NamespaceDeclarationSyntax;
+                    if (parent == null)
+                    {
+                        break;
+                    }
+
+                    // Add the outer namespace as a prefix to the final namespace
+                    nameSpace = $"{parent.Name}.{nameSpace}";
+                    namespaceParent = parent;
+                }
+            }
+
+            // return the final namespace
+            return nameSpace;
         }
     }
 }
