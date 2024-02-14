0daedcfcb40ed7f250221e97e7a281d2302897bb
Michael L Perry <michael@qedcode.com>
2024-02-13 20:42:28 -0600

    Generate a proxy type if the source generator didn't do its job.

--- Jinaga/Serialization/DeserializerCache.cs
+++ Jinaga/Serialization/DeserializerCache.cs
@@ -1,10 +1,11 @@
-﻿using Jinaga.Facts;
+using Jinaga.Facts;
 using System;
 using System.Collections.Immutable;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
+using System.Runtime.InteropServices;
 
 namespace Jinaga.Serialization
 {
@@ -70,8 +71,11 @@ namespace Jinaga.Serialization
                 )
                 .ToArray();
 
+            // If the type does not implement IFactProxy, then try to create a proxy type.
+            Type proxyType = typeof(IFactProxy).IsAssignableFrom(type) ? type : CreateProxyType(type);
+
             // Construct the runtime fact record
-            var factConstructor = type.GetConstructors().Single();
+            var factConstructor = proxyType.GetConstructors().Single();
             var newExpression = Expression.New(
                 factConstructor,
                 parameterExpressions
@@ -240,6 +244,118 @@ namespace Jinaga.Serialization
                 predecessorMultiple
             );
             return deserialize;
+        }
+
+        private static Type CreateProxyType(Type type)
+        {
+            try
+            {
+                var typeSignature = $"{type.Name}Proxy";
+                var assemblyName = new AssemblyName(typeSignature);
+                var assemblyBuilder = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.Run);
+                var moduleBuilder = assemblyBuilder.DefineDynamicModule("MainModule");
+                // Inherit from T
+                var typeBuilder = moduleBuilder.DefineType(typeSignature,
+                        TypeAttributes.Public |
+                        TypeAttributes.Class |
+                        TypeAttributes.AutoClass |
+                        TypeAttributes.AnsiClass |
+                        TypeAttributes.BeforeFieldInit |
+                        TypeAttributes.AutoLayout,
+                        type);
+                // Implement IFactProxy
+                typeBuilder.AddInterfaceImplementation(typeof(IFactProxy));
+                // Define a backing field for the fact graph
+                var fieldBuilder = typeBuilder.DefineField(
+                    "graph",
+                    typeof(FactGraph),
+                    FieldAttributes.Private
+                );
+                // Define a property for the fact graph that explicitly implements IFactProxy
+                var propertyBuilder = typeBuilder.DefineProperty(
+                    "IFactProxy.Graph",
+                    PropertyAttributes.None,
+                    typeof(FactGraph),
+                    Type.EmptyTypes
+                );
+                // Define the getter for the FactReference that explicitly implements IFactProxy
+                var getterBuilder = typeBuilder.DefineMethod(
+                    "IFactProxy.get_Graph",
+                    MethodAttributes.Private |
+                    MethodAttributes.SpecialName |
+                    MethodAttributes.HideBySig |
+                    MethodAttributes.NewSlot |
+                    MethodAttributes.Virtual |
+                    MethodAttributes.Final,
+                    typeof(FactGraph),
+                    Type.EmptyTypes
+                );
+
+                // Implement the getter for the Fact
+                var gil = getterBuilder.GetILGenerator();
+                gil.Emit(OpCodes.Ldarg_0); // Load 'this' onto the stack
+                gil.Emit(OpCodes.Ldfld, fieldBuilder); // Load the value of the 'graph' field onto the stack
+                gil.Emit(OpCodes.Ret); // Return the value on the top of the stack
+
+                propertyBuilder.SetGetMethod(getterBuilder);
+
+                // Define the get_Graph method as an override of the IFactProxy.Graph getter.
+                typeBuilder.DefineMethodOverride(getterBuilder, typeof(IFactProxy).GetProperty(nameof(IFactProxy.Graph)).GetMethod);
+
+                // Define the setter for the FactReference that explicitly implements IFactProxy
+                var setterBuilder = typeBuilder.DefineMethod(
+                    "IFactProxy.set_Graph",
+                    MethodAttributes.Private |
+                    MethodAttributes.SpecialName |
+                    MethodAttributes.HideBySig |
+                    MethodAttributes.NewSlot |
+                    MethodAttributes.Virtual |
+                    MethodAttributes.Final,
+                    typeof(void),
+                    new[] { typeof(FactGraph) }
+                );
+                // Implement the setter for the Fact
+                var sil = setterBuilder.GetILGenerator();
+                sil.Emit(OpCodes.Ldarg_0);
+                sil.Emit(OpCodes.Ldarg_1);
+                sil.Emit(OpCodes.Stfld, fieldBuilder);
+                sil.Emit(OpCodes.Ret);
+                propertyBuilder.SetSetMethod(setterBuilder);
+
+                // Define the set_Graph method as an override of the IFactProxy.Graph setter.
+                typeBuilder.DefineMethodOverride(setterBuilder, typeof(IFactProxy).GetProperty(nameof(IFactProxy.Graph)).SetMethod);
+
+                // Get the only constructor for T
+                var constructor = type.GetConstructors().Single();
+                // Get the constructor parameters for T
+                var parameters = constructor.GetParameters();
+                // Define a constructor for the proxy that takes the same parameters as T
+                var constructorBuilder = typeBuilder.DefineConstructor(
+                    MethodAttributes.Public,
+                    CallingConventions.Standard,
+                    parameters.Select(p => p.ParameterType).ToArray()
+                );
+                // Call the base constructor for T
+                var cil = constructorBuilder.GetILGenerator();
+                cil.Emit(OpCodes.Ldarg_0);
+                for (var i = 0; i < parameters.Length; i++)
+                {
+                    cil.Emit(OpCodes.Ldarg, i + 1);
+                }
+                cil.Emit(OpCodes.Call, constructor);
+                cil.Emit(OpCodes.Ret);
+                var proxyType = typeBuilder.CreateType();
+                return proxyType;
+            }
+            catch (TypeLoadException ex)
+            {
+                throw new ArgumentException($"The type {type.FullName} must be public.", ex);
+            }
+            catch (PlatformNotSupportedException)
+            {
+                // The platform does not support reflection emit.
+                return type;
+            }
         }
     }
 }
