117ab8254091c27de6bf4ec7f88ab50f7eed53d0
Michael L Perry <michael@qedcode.com>
2024-06-26 21:59:38 -0500

    Add Jinaga.Maui

---
+++ Jinaga.Maui/Authentication/OAuth2HttpAuthenticationProvider.cs
@@ -0,0 +1,209 @@
+using Jinaga.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Authentication;
+using Microsoft.Maui.Storage;
+using System.Diagnostics;
+using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text.Json;
+
+namespace Jinaga.Maui.Authentication;
+
+public class OAuth2HttpAuthenticationProvider : IHttpAuthenticationProvider
+{
+    private const string AuthenticationTokenKey = "Jinaga.AuthenticationToken";
+
+    private readonly OAuthClient oauthClient;
+    private readonly ILogger<OAuth2HttpAuthenticationProvider> logger;
+
+    private readonly SemaphoreSlim semaphore = new(1);
+    volatile private AuthenticationToken authenticationToken;
+
+    internal bool IsLoggedIn => authenticationToken != null;
+
+    public OAuth2HttpAuthenticationProvider(OAuthClient oauthClient, ILogger<OAuth2HttpAuthenticationProvider> logger)
+    {
+        this.oauthClient = oauthClient;
+        this.logger = logger;
+    }
+
+    internal void Initialize()
+    {
+        var task = Lock(async () =>
+        {
+            logger.LogInformation("Initializing authentication");
+            await LoadToken().ConfigureAwait(false);
+            if (authenticationToken != null)
+            {
+                // Check for token expiration
+                if (DateTime.TryParse(authenticationToken.ExpiryDate, null, DateTimeStyles.RoundtripKind, out var expiryDate))
+                {
+                    if (DateTime.UtcNow > expiryDate.AddMinutes(-5))
+                    {
+                        var stopwatch = Stopwatch.StartNew();
+                        logger.LogInformation("Refreshing token");
+
+                        await RefreshToken().ConfigureAwait(false);
+                        await SaveToken().ConfigureAwait(false);
+
+                        logger.LogInformation("Token refreshed in {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+                    }
+                    else
+                    {
+                        logger.LogInformation("Cached token is still valid");
+                    }
+                }
+            }
+            else
+            {
+                logger.LogInformation("No cached token");
+            }
+            return true;
+        });
+        task.ContinueWith(t =>
+        {
+            if (t.IsFaulted)
+            {
+                logger.LogError(t.Exception, "Failed to initialize authentication");
+            }
+        });
+    }
+
+    internal Task<bool> Login(string provider)
+    {
+        return Lock(async () =>
+        {
+            logger.LogInformation("Logging in with {Provider}", provider);
+
+            var client = oauthClient;
+            string requestUrl = client.BuildRequestUrl(provider);
+#if WINDOWS
+            var authResult = await WinUIEx.WebAuthenticator.AuthenticateAsync(
+                new Uri(requestUrl),
+                new Uri(client.CallbackUrl)).ConfigureAwait(false);
+#else
+            var authResult = await WebAuthenticator.Default.AuthenticateAsync(
+                new Uri(requestUrl),
+                new Uri(client.CallbackUrl)).ConfigureAwait(false);
+#endif
+            if (authResult == null)
+            {
+                logger.LogInformation("Authentication cancelled");
+                return false;
+            }
+
+            var stopwatch = Stopwatch.StartNew();
+            logger.LogInformation("Received authentication result");
+
+            try
+            {
+                string state = authResult.Properties["state"];
+                string code = authResult.Properties["code"];
+
+                client.ValidateState(state);
+                var tokenResponse = await client.GetTokenResponse(code).ConfigureAwait(false);
+                authenticationToken = ResponseToToken(tokenResponse);
+                await SaveToken().ConfigureAwait(false);
+                logger.LogInformation("Token received in {ElapsedMilliseconds} ms", stopwatch.ElapsedMilliseconds);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "Failed to get token");
+                return false;
+            }
+        });
+    }
+
+    internal Task LogOut()
+    {
+        return Lock(async () =>
+        {
+            logger.LogInformation("Logging out");
+            authenticationToken = null;
+            await SaveToken().ConfigureAwait(false);
+            return true;
+        });
+    }
+
+    public void SetRequestHeaders(HttpRequestHeaders headers)
+    {
+        var cachedAuthenticationToken = authenticationToken;
+        if (cachedAuthenticationToken != null)
+        {
+            headers.Authorization = new AuthenticationHeaderValue("Bearer", cachedAuthenticationToken.AccessToken);
+        }
+    }
+
+    public Task<bool> Reauthenticate()
+    {
+        return Lock(async () =>
+        {
+            if (authenticationToken != null)
+            {
+                await RefreshToken().ConfigureAwait(false);
+                await SaveToken().ConfigureAwait(false);
+                return true;
+            }
+            return false;
+        });
+    }
+
+    private async Task LoadToken()
+    {
+        string tokenJson = await SecureStorage.GetAsync(AuthenticationTokenKey).ConfigureAwait(false);
+        if (tokenJson != null)
+        {
+            AuthenticationToken authenticationToken = JsonSerializer.Deserialize<AuthenticationToken>(tokenJson);
+            this.authenticationToken = authenticationToken;
+        }
+    }
+
+    private async Task SaveToken()
+    {
+        if (authenticationToken == null)
+        {
+            SecureStorage.Remove(AuthenticationTokenKey);
+        }
+        else
+        {
+            string tokenJson = JsonSerializer.Serialize(authenticationToken);
+            await SecureStorage.SetAsync(AuthenticationTokenKey, tokenJson).ConfigureAwait(false);
+        }
+    }
+
+    private async Task RefreshToken()
+    {
+        if (authenticationToken == null)
+        {
+            throw new InvalidOperationException("Attempting to refresh with no token");
+        }
+
+        var tokenResponse = await oauthClient.RequestNewToken(authenticationToken.RefreshToken).ConfigureAwait(false);
+        authenticationToken = tokenResponse == null ? null : ResponseToToken(tokenResponse);
+    }
+
+    private async Task<T> Lock<T>(Func<Task<T>> action)
+    {
+        await semaphore.WaitAsync().ConfigureAwait(false);
+        try
+        {
+            return await action().ConfigureAwait(false);
+        }
+        finally
+        {
+            semaphore.Release();
+        }
+    }
+
+    private static AuthenticationToken ResponseToToken(TokenResponse tokenResponse)
+    {
+        return new AuthenticationToken
+        {
+            AccessToken = tokenResponse.AccessToken,
+            RefreshToken = tokenResponse.RefreshToken,
+            ExpiryDate = DateTime.UtcNow.AddSeconds(tokenResponse.ExpiresIn)
+                .ToString("O", CultureInfo.InvariantCulture)
+        };
+    }
+}
---
+++ Jinaga.Maui/Authentication/OAuthClient.cs
@@ -0,0 +1,160 @@
+﻿using System.Collections.Immutable;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+
+namespace Jinaga.Maui.Authentication;
+
+public class OAuthClient
+{
+    private readonly ImmutableDictionary<string, string> authUrlByProvider;
+    private readonly string accessTokenUrl;
+    private readonly string callbackUrl;
+    private readonly string clientId;
+    private readonly string scope;
+
+    private readonly IHttpClientFactory httpClientFactory;
+
+    private string codeVerifier = "";
+    private string state = "";
+
+    public string CallbackUrl => callbackUrl;
+
+    public OAuthClient(AuthenticationSettings authenticationSettings, IHttpClientFactory httpClientFactory)
+    {
+        authUrlByProvider = authenticationSettings.AuthUrlByProvider;
+        accessTokenUrl = authenticationSettings.AccessTokenUrl;
+        callbackUrl = authenticationSettings.CallbackUrl;
+        clientId = authenticationSettings.ClientId;
+        scope = authenticationSettings.Scope;
+        this.httpClientFactory = httpClientFactory;
+    }
+
+    public string BuildRequestUrl(string provider)
+    {
+        if (accessTokenUrl == null || clientId == null)
+        {
+            throw new Exception("Create a file called Settings.Local.cs and set the AccessTokenUrl and ClientId properties."
+                + " Do not check Settings.Local.cs into source control.");
+        }
+
+        if (!authUrlByProvider.TryGetValue(provider, out var authUrl))
+        {
+            throw new Exception($"No authentication URL for provider {provider}");
+        }
+
+        // Generate random strings for the code verifier and state
+        codeVerifier = GenerateRandomString();
+        state = GenerateRandomString();
+
+        // Hash the code verifier
+        var codeVerifierBytes = Encoding.UTF8.GetBytes(codeVerifier);
+        var codeChallengeBytes = SHA256.HashData(codeVerifierBytes);
+        var codeChallenge = UrlSafeBase64String(codeChallengeBytes);
+
+        // Build the authorization URL
+        var builder = new UriBuilder(authUrl);
+        var urlEncodedCallbackUrl = WebUtility.UrlEncode(callbackUrl);
+        var urlEncodedScope = WebUtility.UrlEncode(scope);
+        builder.Query = $"response_type=code&client_id={clientId}&redirect_uri={urlEncodedCallbackUrl}&scope={urlEncodedScope}&state={state}&code_challenge={codeChallenge}&code_challenge_method=S256";
+        return builder.ToString();
+    }
+
+    public void ValidateState(string receivedState)
+    {
+        if (receivedState != state)
+        {
+            throw new Exception("Failed cross-site request forgery check");
+        }
+    }
+
+    public async Task<TokenResponse> GetTokenResponse(string code)
+    {
+        if (accessTokenUrl == null || clientId == null)
+            throw new Exception("Create a file called Settings.Local.cs and set the AccessTokenUrl and ClientId properties."
+                + " Do not check Settings.Local.cs into source control.");
+
+        // Build the access token request
+        var tokenRequest = new HttpRequestMessage(HttpMethod.Post, accessTokenUrl);
+        tokenRequest.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "grant_type", "authorization_code" },
+            { "client_id", clientId },
+            { "redirect_uri", callbackUrl },
+            { "code_verifier", codeVerifier },
+            { "code", code }
+        });
+
+        // Send the access token request
+        var client = httpClientFactory.CreateClient();
+        client.Timeout = TimeSpan.FromSeconds(30);
+        var tokenResponse = await client.SendAsync(tokenRequest);
+        if (!tokenResponse.IsSuccessStatusCode)
+        {
+            throw new Exception($"Failed to get token response: {(int)tokenResponse.StatusCode} {tokenResponse.ReasonPhrase}");
+        }
+        var tokenContent = await tokenResponse.Content.ReadAsStringAsync();
+
+        // Get the access token
+        var token = JsonSerializer.Deserialize<TokenResponse>(tokenContent);
+        if (token == null)
+        {
+            throw new Exception("Unable to parse token response");
+        }
+
+        return token;
+    }
+
+    public async Task<TokenResponse> RequestNewToken(string refreshToken)
+    {
+        if (accessTokenUrl == null || clientId == null)
+            throw new Exception("Create a file called Settings.Local.cs and set the AccessTokenUrl and ClientId properties."
+                + " Do not check Settings.Local.cs into source control.");
+
+        // Build the refresh request
+        var tokenRequest = new HttpRequestMessage(HttpMethod.Post, accessTokenUrl);
+        tokenRequest.Content = new FormUrlEncodedContent(new Dictionary<string, string>{
+            { "grant_type", "refresh_token" },
+            { "refresh_token", refreshToken },
+            { "client_id", clientId }
+        });
+
+        // Send the refresh request
+        var client = httpClientFactory.CreateClient();
+        client.Timeout = TimeSpan.FromSeconds(30);
+        var tokenResponse = await client.SendAsync(tokenRequest);
+        if (!tokenResponse.IsSuccessStatusCode)
+        {
+            if (tokenResponse.StatusCode == HttpStatusCode.Unauthorized || tokenResponse.StatusCode == HttpStatusCode.BadRequest)
+            {
+                return null;
+            }
+            throw new Exception($"Failed to refresh the token: {(int)tokenResponse.StatusCode} {tokenResponse.ReasonPhrase}");
+        }
+        var tokenContent = await tokenResponse.Content.ReadAsStringAsync();
+
+        // Get the access token
+        var token = JsonSerializer.Deserialize<TokenResponse>(tokenContent);
+        if (token == null)
+        {
+            throw new Exception("Unable to parse token response");
+        }
+
+        return token;
+    }
+
+    private static string GenerateRandomString()
+    {
+        var randomBytes = RandomNumberGenerator.GetBytes(32);
+        return UrlSafeBase64String(randomBytes);
+    }
+
+    private static string UrlSafeBase64String(byte[] bytes)
+    {
+        return Convert.ToBase64String(bytes)
+            .Replace('+', '-')
+            .Replace('/', '_')
+            .Replace("=", "");
+    }
+}
