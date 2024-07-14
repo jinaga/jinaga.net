ea6ad375b29086c4dc3e1af236453c718c3b5805
Michael L Perry <michael@qedcode.com>
2024-07-14 18:15:26 -0500

    Inject AuthenticationService by interface

--- Jinaga/Http/IHttpAuthenticationProvider.cs
+++ Jinaga/Http/IHttpAuthenticationProvider.cs
@@ -3,9 +3,20 @@ using System.Threading.Tasks;
 
 namespace Jinaga.Http
 {
+    /// <summary>
+    /// Adds authentication details to HTTP requests.
+    /// </summary>
     public interface IHttpAuthenticationProvider
     {
+        /// <summary>
+        /// Set the authentication token in the request headers.
+        /// </summary>
+        /// <param name="headers">HTTP headers to modify</param>
         void SetRequestHeaders(HttpRequestHeaders headers);
+        /// <summary>
+        /// Reauthenticate the user. Called when a request fails with a 401 Unauthorized status.
+        /// </summary>
+        /// <returns>True if the token was successfully refreshed</returns>
         Task<JinagaAuthenticationState> Reauthenticate();
     }
 }
