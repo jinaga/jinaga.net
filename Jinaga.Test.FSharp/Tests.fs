bb6cf7c5d3cea11601c3b5a68609133feebcda35
Michael L Perry <michael@qedcode.com>
2024-09-21 19:23:00 -0500

    Reproduce exception from issue

--- Jinaga.Test.FSharp/Tests.fs
+++ Jinaga.Test.FSharp/Tests.fs
@@ -1,8 +1,35 @@
 module Tests
 
 open System
+open System.Linq
+open Jinaga
 open Xunit
+
+[<FactType("Corporate.Company")>]
+type Company =
+  {
+    Identifier : string
+  }
+
+[<FactType("Corporate.Employee")>]
+type Employee =
+  {
+    Company : Company
+    EmployeeNumber : int
+  }
+
 
 [<Fact>]
 let ``My test`` () =
-    Assert.True(true)
+    let employeesOfCompany =
+        Given<Company>.Match(
+            (fun (company : Company) (facts : Repository.FactRepository) ->
+            facts.OfType<Employee>().Where(fun x -> x.Company = company))
+        )
+
+    let j = JinagaClient.Create()
+
+    let employees = j.Query(employeesOfCompany, { Identifier = "Contoso" }).Result
+
+    for employee in employees do
+        printfn $"%A{employee}"
