d6ec3087a316173568795ee353ea04fed369cb8d
Michael L Perry <michael@qedcode.com>
2024-09-23 20:23:41 -0500

    Translate Expr to Expression recursively

--- Jinaga.Test.FSharp/Tests.fs
+++ Jinaga.Test.FSharp/Tests.fs
@@ -1,10 +1,15 @@
 module Tests
 
-open System
-open System.Linq
+open System.Linq.Expressions
+open Microsoft.FSharp.Quotations
 open Jinaga
 open Xunit
-open System.Linq.Expressions
+open Microsoft.FSharp.Linq.RuntimeHelpers
+open Jinaga.Projections
+open System.Collections.Immutable
+open System
+open Jinaga.Repository
+open System.Linq
 
 [<FactType("Corporate.Company")>]
 type Company =
@@ -19,13 +24,79 @@ type Employee =
         EmployeeNumber : int
     }
 
+let rec translateExpression (expr: Expr) : Expression =
+    match expr with
+    | Patterns.Call(instance, methodInfo, args) -> 
+        let translatedInstance = 
+            instance |> Option.map translateExpression |> Option.toObj
+        let translatedArgs = args |> List.map translateExpression |> List.toArray
+        Expression.Call(translatedInstance, methodInfo, translatedArgs)
+    
+    | Patterns.Lambda(param, body) -> 
+        let translatedParam = translateParameter param
+        let translatedBody = translateExpression body
+        Expression.Lambda(translatedBody, translatedParam)
+
+    | Patterns.Let(var, valueExpr, bodyExpr) -> 
+        // Inline the "let" into an expression tree as there is no direct mapping in C#
+        let translatedVar = translateParameter var
+        let translatedValue = translateExpression valueExpr
+        let translatedBody = translateExpression bodyExpr
+        Expression.Invoke(
+            Expression.Lambda(translatedBody, translatedVar), 
+            translatedValue)
+
+    | Patterns.PropertyGet(instance, propertyInfo, _) -> 
+        let translatedInstance = instance |> Option.map translateExpression |> Option.toObj
+        Expression.Property(translatedInstance, propertyInfo)
+    
+    | Patterns.Value(value, _) ->
+        Expression.Constant(value)
+
+    | Patterns.LetRecursive _ -> failwith "LetRecursive is not supported in C# expression trees"
+    
+    | _ -> failwithf "Unsupported expression type: %A" expr
+
+and translateLambda (expr: Expr) : LambdaExpression =
+    match expr with
+    | Patterns.Lambda(param, body) -> 
+        let translatedParam = translateParameter param
+        let translatedBody = translateExpression body
+        Expression.Lambda(translatedBody, translatedParam)
+    | _ -> failwith "Expected a lambda expression"
+
+and translateParameter (param: Var) : ParameterExpression =
+    Expression.Parameter(param.Type, param.Name)
+
+type Given2<'TFact when 'TFact : not struct>() =
+    static member Match<'TProjection>(specExpression: Expr<'TFact -> FactRepository -> IQueryable<'TProjection>>) : Specification<'TFact, 'TProjection> =
+        let struct (givens, matches, projection) = SpecificationProcessor.Queryable<'TProjection>(translateLambda specExpression)
+        let specificationGivens = 
+            givens
+            |> Seq.map (fun g -> SpecificationGiven(g, ImmutableList<ExistentialCondition>.Empty))
+            |> ImmutableList.ToImmutableList
+        Specification<'TFact, 'TProjection>(specificationGivens, matches, projection)
+
+    static member Select<'TProjection>(specSelector: Expr<'TFact -> FactRepository -> 'TProjection>) : Specification<'TFact, 'TProjection> =
+        let lambdaExpression = LeafExpressionConverter.QuotationToLambdaExpression(specSelector)
+        let struct (givens, matches, projection) = SpecificationProcessor.Select<'TProjection>(lambdaExpression)
+        let specificationGivens = 
+            givens
+            |> Seq.map (fun g -> SpecificationGiven(g, ImmutableList<ExistentialCondition>.Empty))
+            |> ImmutableList.ToImmutableList
+        Specification<'TFact, 'TProjection>(specificationGivens, matches, projection)
+
 
 [<Fact>]
 let ``My test`` () =
     let employeesOfCompany =
-        Given<Company>.Match<Employee>(
-            fun company facts ->
-                facts.OfType<Employee>(fun x -> x.Company = company)
+        Given2<Company>.Match<Employee>(
+            <@ fun company facts ->
+                query {
+                    for employee in facts.OfType<Employee>() do
+                    where (employee.Company = company)
+                    select employee
+                } @>
         )
 
     let j = JinagaClient.Create()
