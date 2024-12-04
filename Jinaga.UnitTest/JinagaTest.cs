8a8fb450abf2dd5bcc7e91625d57c7d058f5dc7c
Michael L Perry <michael@qedcode.com>
2024-12-03 22:01:32 -0600

    But only when possible

--- Jinaga.UnitTest/JinagaTest.cs
+++ Jinaga.UnitTest/JinagaTest.cs
@@ -1,6 +1,8 @@
+using Jinaga.Projections;
 using Jinaga.Storage;
 using Microsoft.Extensions.Logging.Abstractions;
 using System;
+using System.Collections.Immutable;
 
 namespace Jinaga.UnitTest
 {
@@ -23,7 +25,7 @@ namespace Jinaga.UnitTest
             var loggerFactory = NullLoggerFactory.Instance;
             var network = new SimulatedNetwork(
                 options.User == null ? null : options.User.publicKey);
-            var client = new JinagaClient(new MemoryStore(), network, [], loggerFactory);
+            var client = new JinagaClient(new MemoryStore(), network, ImmutableList<Specification>.Empty, loggerFactory);
             return client;
         }
     }
