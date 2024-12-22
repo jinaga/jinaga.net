13a40300f061cd265b583f6262a5457772d5135c
Michael L Perry <michael@qedcode.com>
2024-12-22 13:03:45 -0600

    Test describing purge conditions

---
+++ Jinaga.Test/Purge/DescribePurgeConditionsTest.cs
@@ -0,0 +1,65 @@
+using Jinaga.Test.Model.Order;
+
+namespace Jinaga.Test.Purge;
+
+public class PurgeConditionsTest
+{
+    [Fact]
+    public void PurgeConditions_CanDescribeEmpty()
+    {
+        var description = PurgeConditions.Describe(pc => pc);
+        string expected =
+            """
+            purge {
+            }
+
+            """;
+        description.Should().Be(expected);
+    }
+
+    [Fact]
+    public void PurgeConditions_CanDescribeSingleCondition()
+    {
+        var description = PurgeConditions.Describe(pc => pc
+            .Purge<Order>().WhenExists<OrderCancelled>(cancelled => cancelled.order)
+        );
+        string expected =
+            """
+            purge {
+                (fact: Order) {
+                    cancelled: Order.Cancelled [
+                        cancelled->order: Order = fact
+                    ]
+                } => cancelled
+            }
+
+            """;
+        description.Should().Be(expected);
+    }
+
+    [Fact]
+    public void PurgeConditions_CanDescribeMultipleConditions()
+    {
+        var description = PurgeConditions.Describe(pc => pc
+            .Purge<Order>().WhenExists<OrderCancelled>(cancelled => cancelled.order)
+            .Purge<Order>().WhenExists<OrderShipped>(shipped => shipped.order)
+        );
+        string expected =
+            """
+            purge {
+                (fact: Order) {
+                    cancelled: Order.Cancelled [
+                        cancelled->order: Order = fact
+                    ]
+                } => cancelled
+                (fact: Order) {
+                    shipped: Order.Shipped [
+                        shipped->order: Order = fact
+                    ]
+                } => shipped
+            }
+
+            """;
+        description.Should().Be(expected);
+    }
+}
